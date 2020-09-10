921e1f829dd4a21f9de94d38437c68aed8b49c25
antoStif <abopeto1@gmail.com>
2020-09-10 11:17:05 +0100

    First Commit

---
+++ app/src/main/java/com/levagency/sanjola/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.levagency.sanjola
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
