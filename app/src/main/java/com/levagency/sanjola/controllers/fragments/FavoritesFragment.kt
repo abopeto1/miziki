53b511d032870d6468c9248dc540568607889837
antoStif <abopeto1@gmail.com>
2020-09-11 12:50:15 +0100

    Add Favorites Fragment and Delete AppActivity File and nested files

--- app/src/main/java/com/levagency/sanjola/controllers/fragments/FavoritesFragment.kt
+++ app/src/main/java/com/levagency/sanjola/controllers/fragments/FavoritesFragment.kt
@@ -6,20 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
 import com.levagency.sanjola.R
-import com.levagency.sanjola.databinding.FragmentMusicBinding
+import com.levagency.sanjola.databinding.FragmentFavoritesBinding
 
-class MusicFragment : Fragment() {
+class FavoritesFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding = DataBindingUtil.inflate<FragmentMusicBinding>(inflater, R.layout.fragment_music, container, false)
-        binding.btnNext.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_musicFragment_to_favoritesFragment)
-        }
+        val binding = DataBindingUtil.inflate<FragmentFavoritesBinding>(inflater, R.layout.fragment_favorites, container, false)
+
         return binding.root
     }
 }
