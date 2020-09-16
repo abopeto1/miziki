739e9b7bf4afe13d170e1c2a17efc4bf96b16256
antoStif <abopeto1@gmail.com>
2020-09-16 22:46:03 +0100

    Add ViewHolder Class for TileLayout

--- app/src/main/java/com/levagency/miziki/adapters/AlbumAdapter.kt
+++ app/src/main/java/com/levagency/miziki/adapters/AlbumAdapter.kt
@@ -1,7 +1,9 @@
 package com.levagency.miziki.adapters
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.levagency.miziki.R
@@ -9,24 +11,30 @@ import com.levagency.miziki.models.Album
 import com.levagency.miziki.utils.TileItemViewHolder
 import timber.log.Timber
 
-class AlbumAdapter : RecyclerView.Adapter<TileItemViewHolder>() {
+class AlbumAdapter : RecyclerView.Adapter<AlbumAdapter.ViewHolder>() {
     var data = listOf<Album>()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TileItemViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.tile_item_view, parent, false) as TextView
-
-        return TileItemViewHolder(view)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    {
+        val imageAlbum: ImageView = itemView.findViewById(R.id.album_image)
+        val albumTitle: TextView = itemView.findViewById(R.id.album_title)
     }
 
-    override fun onBindViewHolder(holder: TileItemViewHolder, position: Int) {
-        val item = data[position]
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.tile_item_view, parent, false)
 
-        holder.textView.text = item.name
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = data[position]
+//        val res = holder.itemView.context.resources
+        holder.albumTitle.text = item.name
     }
 
     override fun getItemCount() = data.size
