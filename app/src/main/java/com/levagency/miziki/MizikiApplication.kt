81732048c4cde3539e401c0b9e4c64003e3a3748
Arnold Bopeto <abopeto1@gmail.com>
2020-09-29 19:31:10 +0100

    Create Browser Fragment

--- app/src/main/java/com/levagency/miziki/MizikiApplication.kt
+++ app/src/main/java/com/levagency/miziki/MizikiApplication.kt
@@ -1,12 +1,53 @@
 package com.levagency.miziki
 
 import android.app.Application
+import android.os.Build
+import androidx.work.*
+import com.levagency.miziki.domain.album.worker.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class MizikiApplication : Application() {
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
     override fun onCreate() {
         super.onCreate()
+        delayedInit()
+    }
 
-        Timber.plant(Timber.DebugTree())
+    private fun delayedInit() {
+        applicationScope.launch {
+            Timber.plant(Timber.DebugTree())
+            setupRecurringWork()
+        }
+    }
+
+    /**
+     * Setup WorkManager background job to fetch new network data daily
+     */
+    private fun setupRecurringWork(){
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
+                }
+            }
+            .build()
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+
+        Timber.d("Periodic Work request for sync is scheduled")
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
     }
 }
